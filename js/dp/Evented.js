b32837df937832c238f43a686b306ebb9e966328
pwoloszun <piotr.woloszun@gmail.com>
2014-05-07 11:11:52 +0200

    v1.4

--- js/dp/Evented.js
+++ js/dp/Evented.js
@@ -1,8 +1,12 @@
 // Implement Evented module
 function eventedExampleUsage() {
-  var evented = new Evented();
+  var evented = {name: "Bob"};
+  mixin(evented, Evented);
   evented.on("remove", function() {
     log("remove triggered", arguments);
+  });
+  evented.on("remove", function() {
+    log("2nd remove triggered", arguments);
   });
   evented.trigger("remove", [1, "qq"]); // loged msg on console
   evented.off("remove");
