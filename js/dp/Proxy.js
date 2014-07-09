695cb9645fce29bb1476a1f255a7f9f92baabdb1
pwoloszun <piotr.woloszun@gmail.com>
2014-07-09 13:04:53 +0200

    v1.8

--- js/dp/Proxy.js
+++ js/dp/Proxy.js
@@ -1,17 +1,27 @@
 /*
-* Using userRepository create cachedUserRepository - it should return cached results
-* if previous request has been made earlier than 1min ago
+* Using userRepository create cachedUserRepository -
+* it should cache results for up to 5 seconds, and send AJAX
+* request only if there are no cached results or cache has expired
 * */
 function proxyTest(cachedUserRepository) {
   var allUsers, user;
 
+  // sends aJAX request
   cachedUserRepository.findAll(function(users) {
     assertEqual(users.length, 3);
   });
 
   setTimeout(function() {
+    // returns cached results
     cachedUserRepository.findAll(function(users) {
       assertEqual(users.length, 3);
     });
   }, 3000);
+
+  setTimeout(function() {
+    // sends aJAX request
+    cachedUserRepository.findAll(function(users) {
+      assertEqual(users.length, 3);
+    });
+  }, 6000);
 }
