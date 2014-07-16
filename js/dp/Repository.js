9a1e8a2450d790e5a4b7214f6826625a0c65251a
pwoloszun <piotr.woloszun@gmail.com>
2014-07-16 11:42:43 +0200

    v 1.9 - improved repo task

--- js/dp/Repository.js
+++ js/dp/Repository.js
@@ -7,19 +7,32 @@
 //});
 
 function repositoryTest(usersRepository) {
-  var allUsers, user;
+  var allUsers, user, hasBeenCalled = {
+    findAll: false,
+    findBy2: false,
+    findBy997: false
+  };
 
   usersRepository.findAll(function(users) {
     assertEqual(users.length, 3);
+    hasBeenCalled.findAll = true;
   });
 
   usersRepository.findById(2, function(user) {
     assertEqual(user.name, "Batman");
+    hasBeenCalled.findBy2 = true;
   });
 
   usersRepository.findById(997, function(user) {
     assertEqual(user, undefined);
+    hasBeenCalled.findBy997 = true;
   });
+
+  setTimeout(function() {
+    assertEqual(hasBeenCalled.findAll, true);
+    assertEqual(hasBeenCalled.findBy2, true);
+    assertEqual(hasBeenCalled.findBy997, true);
+  }, 2000);
 }
 
 //repositoryTest(usersRepository);
