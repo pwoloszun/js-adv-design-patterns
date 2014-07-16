e6b216c13762214f735333d89f7f98d25d3f7beb
pwoloszun <piotr.woloszun@gmail.com>
2014-07-16 11:34:21 +0200

    v 1.9 - added Timer app

---
+++ js/models/Timer.js
@@ -0,0 +1,7 @@
+// var timeoutId = setTimeout(fn, delay);
+// clearTimeout(timeoutId);
+
+// var intervalId = setInterval(fn, delay);
+// clearInterval(intervalId);
+// TODO: define Timer class using module pattern,
+// reuse modules: mixin, Evented
