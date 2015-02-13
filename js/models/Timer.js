bad80b414b4c4b8622b47bf5ab9bf3ef1b2d8675
Piotr Woloszun <piotr.woloszun@gmail.com>
2015-02-13 07:46:56 +0100

    Timer additional comments

--- js/models/Timer.js
+++ js/models/Timer.js
@@ -5,3 +5,7 @@
 // clearInterval(intervalId);
 // TODO: define Timer class using module pattern,
 // reuse modules: mixin, Evented
+
+//setTimeout(function() {
+// TODO WARNING: this === window ALWAYS!
+//});
