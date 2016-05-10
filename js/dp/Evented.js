75a400976dc9a6e727d82acf197e5cad97638d1d
Piotr Woloszun <piotr.woloszun@gmail.com>
2016-05-10 14:37:11 +0200

    Evented task updt

--- js/dp/Evented.js
+++ js/dp/Evented.js
@@ -7,21 +7,25 @@ function eventedExampleUsage(mixin, Evented) {
   var bob = {name: "Bob"};
   mixin(bob, Evented);
 
-  bob.on("remove", function(number, str) {
+  bob.on("xxx", function(number, str) {
     log(this.name, "remove triggered", arguments);
   });
 
-  bob.on("remove", function() {
+  bob.on("xxx", function() {
     log(this.name, "2nd remove triggered", arguments);
   });
 
-  bob.trigger("remove", [1, "qq"]); // logged msg on console
+  bob.on("yyy", function(number, str) {
+    log(this.name, "YYY was triggered", arguments);
+  });
+
+  bob.trigger("xxx", [1, 123, -997]); // logged msg on console
 
   var ed = {name: "Ed"};
   mixin(ed, Evented);
-  ed.trigger("remove"); // nothing happens
+  ed.trigger("xxx"); // nothing happens
 
-  bob.off("remove");
-  bob.trigger("remove", [{}, 997.001]); // nothing logged
+  // bob.off("xxx");
+  bob.trigger("xxx", [{}, 997.001]); // nothing logged
 }
 //eventedExampleUsage(mixin, Evented);
